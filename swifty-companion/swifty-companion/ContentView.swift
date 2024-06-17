a2c716006c7814876eae825b1b54a5f9c7103d74
ankasamanyan <anikakasamanyan@gmail.com>
2024-06-17 16:08:46 +0200

    init

---
+++ swifty-companion/swifty-companion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swifty-companion
+//
+//  Created by Anait Kasamanian on 17.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swifty-companion/swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Anait Kasamanian on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
