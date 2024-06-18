5391b0ec8ace20aa2761c14fb6299cf49cc57f38
ankasamanyan <anikakasamanyan@gmail.com>
2024-06-18 18:16:10 +0200

    feat: log in View ui added

--- swifty-companion/swifty-companion/LoginView.swift
+++ swifty-companion/swifty-companion/LoginView.swift
@@ -5,24 +5,47 @@
 //  Created by Anait Kasamanian on 17.06.24.
 //
 
+import OAuthSwift
 import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            // Add login UI elements here
-            Text("Login View")
+        ZStack {
+            Color.indigo
+                .opacity(0.97)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Welcome to Swifty Companion!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.top, 90)
+                
+                Spacer()
+                Image("computerGirl")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 350)
+                
+                Spacer()
+                Button(action: {
+                }) {
+                    Text("Log In With 42")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.indigo)
+                        .padding(.horizontal, 85)
+                        .padding(.vertical, 15)
+                        .background(Color.white.opacity(0.8))
+                        .cornerRadius(10)
+                }
+                .padding(.bottom, 50)
+            }
+            .padding()
         }
-        .navigationTitle("Login")
     }
 }
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
-
 
 #Preview {
     LoginView()
